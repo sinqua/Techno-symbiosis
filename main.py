fb363121308a15abd584ad831007043b489fb16a
macnoll <quashine@icloud.com>
2024-06-18 17:49:15 +0900

    rough draft

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello World!")
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os 
+from supabase import create_client, Client
+
+# todo : get the key from env file
+# url: str = os.environ.get("https://cwiespeponefxujkahlm.supabase.co")
+# key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3aWVzcGVwb25lZnh1amthaGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2OTg3ODUsImV4cCI6MjAzNDI3NDc4NX0.rGSmo-gYeCnNFoYfN_0fJsE_z7knoAxYU9BoiDduP5c")
+
+
+url: str = "https://cwiespeponefxujkahlm.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3aWVzcGVwb25lZnh1amthaGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2OTg3ODUsImV4cCI6MjAzNDI3NDc4NX0.rGSmo-gYeCnNFoYfN_0fJsE_z7knoAxYU9BoiDduP5c"
+supabase: Client = create_client(url, key)
+
+
+
+
+llm = Ollama(model="llama3")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a world class techno-symbiosis master. You are professor at MIT."),
+    ("user", "{input}")
+])
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+# todo:  make function to take input from user
+message = chain.invoke({"input": "how can langsmith help with testing?"})
+
+
+
+
+response = supabase.table("draft").select("*").execute()
+
+print(response)
+
+data = supabase.table("draft").insert([{"name": "Hello, how are you?"}]).execute()
+
+print(data)

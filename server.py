47f708ed14fdb9eec82304b774a5a634203e8cf0
규화 신 <quashine@icloud.com>
2024-11-07 15:45:20 +0900

    new ssl

--- server.py
+++ server.py
@@ -40,4 +40,4 @@ def hello_image():
     return message
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
